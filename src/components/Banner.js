dcc16d9f03dcbe2ea63c2a8e2756df57226b4eb2
KarthikSelvaraj28 <karthikrajcsk22@gmail.com>
2025-07-18 10:37:12 +0530

    updated

--- src/components/Banner.js
+++ src/components/Banner.js
@@ -1,15 +1,54 @@
+// components/Banner.js
 import React from 'react';
-import './Banner.css'; // Import the CSS file for animation
+import Slider from 'react-slick';
+import { Box } from '@mui/material';
+import 'slick-carousel/slick/slick.css';
+import 'slick-carousel/slick/slick-theme.css';
+
+const bannerImages = [
+  '/images/bannerbg.png',
+  '/images/karbanner.png',
+  
+];
 
 const Banner = () => {
+  const settings = {
+    dots: true,
+    infinite: true,
+    speed: 2000,
+    autoplay: true,
+    autoplaySpeed: 3000,
+    slidesToShow: 1,
+    slidesToScroll: 1,
+    arrows: false,
+    fade: true,
+    pauseOnHover: false,
+  };
+
   return (
-    <div className="bannerContainer">
-      <img
-        src="/images/karbanner.png"
-        alt="KCR Transport Banner"
-        className="bannerImage"
-      />
-    </div>
+    <Box
+      sx={{
+        width: '100%',
+        height: { xs: '250px', sm: '400px', md: '600px' },
+        overflow: 'hidden',
+      }}
+    >
+      <Slider {...settings}>
+        {bannerImages.map((img, index) => (
+          <Box
+            key={index}
+            component="img"
+            src={img}
+            alt={`Banner ${index}`}
+            sx={{
+              width: '100%',
+              height: '100%',
+              objectFit: 'cover',
+            }}
+          />
+        ))}
+      </Slider>
+    </Box>
   );
 };
 
